c7c3a9831bb33a3f7a303fa95de915e3478c944f
akashm-2003 <akashmanna318@gmail.com>
2023-11-02 19:36:10 +0530

    Changed the URI spelling

--- data/database.js
+++ data/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB=()=>{ mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     dbName: "toDoList",
   })
   .then(() => {
