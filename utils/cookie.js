cbcf0dcc95c9de105113a496fbdbe033e2e481f3
akashm-2003 <akashmanna318@gmail.com>
2023-11-04 01:40:46 +0530

    updated cookie time

--- utils/cookie.js
+++ utils/cookie.js
@@ -6,7 +6,7 @@ export const createCookie = (user, res, message, statusCode = 201) => {
     .status(statusCode)
     .cookie("token", token, {
       httpOnly: true,
-      maxAge: 15 * 60 * 1000,
+      maxAge: 30*24*60 * 60 * 1000,
       // SameSite none means allow to use cookie in cross site (different domain of frontend and backend)
       sameSite:  process.env.NODE_ENV === "development" ?"lax":"none",
       // Secure means allow to use cookie in https
